87a48720782e4b8e1cb7f5f4c49e6c77fbb1bdf7
tatsuyamoriguchi <caliosdev@gmail.com>
2019-09-27 09:43:21 -0700

    Initial Commit

---
+++ RESTAPISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RESTAPISample
+//
+//  Created by Tatsuya Moriguchi on 9/27/19.
+//  Copyright © 2019 Becko's Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
